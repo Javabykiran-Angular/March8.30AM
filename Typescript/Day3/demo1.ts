5ae22fa287a7b9138d898e316ca2665cfa70ffb2
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-03-12 09:48:49 +0530

    Day3

---
+++ Typescript/Day3/demo1.ts
@@ -0,0 +1,40 @@
+
+//operators
+//assisgnment = +=
+// relational => ==, => =< < > !=
+// Arithemathic => +,*,-,/,%
+// logical => && || != 
+// ternary operator => condition?expression1:expression2
+// bitwise => &, | ^ ! ~ << >>
+// incr & decr= > pre & post ++a a++ --a a--
+
+var temp=21<4?'Hi':'bye';
+// console.log("Value of temp is "+temp);
+
+//Loops
+
+// if , if-else switch,break , continue
+// while, do-while , for, foreach
+
+if(2<4){
+    // console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+for(let i=0;i<4;i++){
+    // console.log("Value of i "+i);
+}
+// var has global scope
+// let has scope  nearest within that block {}
+// console.log("Value of Outside the loop "+i);
+
+const j=12;
+// const has global scope
+
+var b=4;
+while(b!=0){
+// console.log("Value of b is "+b);
+b--;
+}
+
---
+++ Typescript/Day3/demo2.js
@@ -0,0 +1,18 @@
+//function
+// 1 function without argum. & without return type
+// 2 function with argm & without return type
+// 3 function without argum. & with return type
+// 4 function with argm & with return type
+function add() {
+    console.log("U are In Function");
+}
+// add();
+function add1(a, b) {
+    return (a + b);
+}
+// var temp=add1(2,3);
+// console.log("Addition is "+temp);
+function add3(a, b) {
+    console.log("Addition is " + (a + b));
+}
+add3(4, 4);
