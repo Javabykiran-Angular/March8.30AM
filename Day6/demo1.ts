aa40fe10c2e8e428643deff1856606a4c94c9184
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-03-17 09:50:06 +0530

    Day6

---
+++ Day6/demo1.ts
@@ -0,0 +1,6 @@
+
+var a1:number[]=[22,33,44,55,66,77,6,9,34];
+//slice => It copied a data from original array from a section
+var temp=a1.slice(1,6)
+console.log("Original Data "+a1);
+console.log("Copied Data "+temp);
